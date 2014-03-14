7d31804ce7736440ccabe63f85283c02e397ba8d
PsypherPunk <psypherpunk@gmail.com>
2014-03-14 19:47:37 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name="python-tweet-archive",
-	version="0.0.1",
+	version="1.0.1",
 	author="PsypherPunk",
 	author_email="psypherpunk@gmail.com",
 	packages=[ "tweetarchive" ],
